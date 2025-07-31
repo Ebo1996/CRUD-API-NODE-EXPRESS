a7ed6d6429abcbf2006752fddd875001ee510614
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-07-31 10:44:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Product
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node API Server');
+});
+app.post('/api/products', (req, res) => { res.send(req.body) });
+
 
 mongoose.connect("mongodb+srv://ebisaberhanu1996:u0eVtJQ0hTb4t5a6@crud.io8xdkz.mongodb.net/Node-API?retryWrites=true&w=majority&appName=CRUD")
   .then(() => console.log('Connected to MongoDB!'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
-app.get('/', (req, res) => {
-  res.send('Hello from Node API Server');
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
