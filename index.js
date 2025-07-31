29de39ab043a16cd64d90914c74b65e7627728ba
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-07-31 13:41:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,21 +9,26 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello from Node API Server');
 });
-app.post('/api/products', async (req, res) => {
-  const product = await Product.create(req.body);
-  res.status(200).json(product);
-  try {
 
-  }
-  catch (error) {
+app.get('/api/products', async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
     res.status(500).json({ message: error.message });
   }
-
-
-
-
-
 });
+
+
+app.post('/api/products', async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 
 
 mongoose.connect("mongodb+srv://ebisaberhanu1996:u0eVtJQ0hTb4t5a6@crud.io8xdkz.mongodb.net/Node-API?retryWrites=true&w=majority&appName=CRUD")
